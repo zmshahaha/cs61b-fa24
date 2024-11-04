5ce72fef4ca12fd35f7287334bf54d65b4ecc684
maosheng zhao <zmshahaha@gmail.com>
2024-11-05 04:34:35 +0800

    lab03

--- lab03/src/adventure/MachineStage.java
+++ lab03/src/adventure/MachineStage.java
@@ -78,26 +78,18 @@ public class MachineStage implements AdventureStage {
     }
 
     public static int mysteryMax(int a, int b) {
-        int w = (b - a) >> 31;
-        int z = ~(b - a) >> 31;
+        int w = ~(b - a) >> 31;
+        int z = (b - a) >> 31;
 
-        int max = b & w | a & z;
-        return max;
+        int max;
+        if (a > b) {
+            return a;
+        }
+        return b;
     }
 
     public static int mysteryAdd(int a, int b) {
-        int x = a, y = b;
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a + b;
     }
 
     /**
@@ -126,7 +118,7 @@ public class MachineStage implements AdventureStage {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + mysteryAdd(sum, x[i]);
+            sum = mysteryAdd(sum, x[i]);
             i = i + 1;
         }
         return sum;
--- lab03/src/adventure/PalindromeStage.java
+++ lab03/src/adventure/PalindromeStage.java
@@ -68,7 +68,7 @@ public class PalindromeStage implements AdventureStage {
             reversed = new IntList(l.first, reversed);
             l = l.rest;
         }
-        return reversed;
+        return new IntList(l.first, reversed);
     }
 
     /**
@@ -77,8 +77,8 @@ public class PalindromeStage implements AdventureStage {
      */
     private static IntList digitsToIntList(String s) {
         int[] a = new int[s.length()];
-        for (int i = s.length(); i > 0; i++) {
-            a[s.length() - i] = Character.getNumericValue(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            a[i] = Character.getNumericValue(s.charAt(i));
         }
         return IntList.of(a);
     }
